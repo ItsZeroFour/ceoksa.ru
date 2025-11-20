312f1a2e859caefdf6a8e4a3f6ebfad4ea42510a
ItsZeroFour <itsZeroFourX@gmail.com>
2025-11-20 12:48:58 +0300

    add banner

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
+import Main from "./pages/main/Main";
 
 function App() {
   return (
     <div className="App">
       <div className="wrapper">
         <Header />
+
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
       </div>
     </div>
   );
