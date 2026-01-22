60c54a6e23a90b69364d0bc6935a2690a9fd8d95
itsZeroFour <itsZeroFourX@gmail.com>
2026-01-22 18:24:54 +0300

    add link

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -3,6 +3,7 @@ import style from "./footer.module.scss";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchFooter } from "../../redux/slices/strapi/footerSlice";
+import DOMPurify from "dompurify";
 
 const Footer = () => {
   const dispatch = useDispatch();
@@ -15,14 +16,21 @@ const Footer = () => {
 
   const isDataReady = Boolean(status === "succeeded" && data?.text);
 
+  const cleanHTML = DOMPurify.sanitize(isDataReady && data.text, {
+    ALLOWED_TAGS: ["p", "a", "br", "strong", "em", "div", "span"],
+    ALLOWED_ATTR: ["href", "target", "rel"],
+  });
+
   return (
     <footer className={style.footer}>
       <div className="container">
         {isDataReady && (
           <div className={style.footer__wrappe}>
-            <p>{data.text}</p>
+            <p dangerouslySetInnerHTML={{ __html: cleanHTML }} />
 
-            <Link to="/polzovatelskoe-soglashenie">Пользовательское соглашение</Link>
+            <Link to="/polzovatelskoe-soglashenie">
+              Пользовательское соглашение
+            </Link>
             <Link to="/privacy-policy">Политика конфиденциальности</Link>
           </div>
         )}
